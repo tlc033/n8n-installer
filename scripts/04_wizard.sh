6517186226cb0d5ac79683e9636cdbe8a147954e
Yury Kossakovsky <kossakovskie@gmail.com>
2025-08-19 14:01:02 -0600

    Increase the number of options in the whiptail checklist for the service selection wizard to enhance user experience

--- scripts/04_wizard.sh
+++ scripts/04_wizard.sh
@@ -104,7 +104,7 @@ done
 
 # Use whiptail to display the checklist
 CHOICES=$(whiptail --title "Service Selection Wizard" --checklist \
-  "Choose the services you want to deploy.\nUse ARROW KEYS to navigate, SPACEBAR to select/deselect, ENTER to confirm." 32 90 19 \
+  "Choose the services you want to deploy.\nUse ARROW KEYS to navigate, SPACEBAR to select/deselect, ENTER to confirm." 32 90 20 \
   "${services[@]}" \
   3>&1 1>&2 2>&3)
 
